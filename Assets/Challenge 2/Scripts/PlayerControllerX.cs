dd55f2b1a3263f824129a895c82627e6ef2959ab
JohnItalo05 <simonebersani2005@gmail.com>
2025-10-31 12:39:40 +0100

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool readyToSpawn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (readyToSpawn)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                readyToSpawn = false;
+                Invoke("BecomeReady", 0.3f);
+            }
+        }
+    }
+    void BecomeReady()
+    {
+        readyToSpawn = true;
+    }
+}
